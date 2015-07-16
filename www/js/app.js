de997429190abf717e433ea5f2b07aa1da17e07a
hugosenari <hugosenari@gmail.com>
2015-07-16 13:05:06 -0200

    basic init show settings, does nothing yet

--- www/js/app.js
+++ www/js/app.js
@@ -1,4 +1,22 @@
 (function(){
 	'use strict';
-	angular.module('ttRssMobilete', ['onsen.directives']);
+	var module = ons.bootstrap('ttRssMobilete', [
+		'onsen',
+		'onsen.directives'
+	]),
+	sid = localStorage.getItem('sid'),
+	settings = localStorage.getItem('settings');
+	
+	module.controller('AppController', function($scope) {
+	});
+	
+	module.controller('LoadingController', function($scope) {
+		if (sid) {
+			$scope.guidon.pushPage('categories.html', {});
+		} else if (settings) {
+			$scope.guidon.pushPage('login.html', {});
+		} else {
+			$scope.guidon.pushPage('settings.html', {});
+		}
+	});
 })();

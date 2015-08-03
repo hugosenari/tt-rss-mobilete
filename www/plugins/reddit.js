bbb3a6582ebd66e1c7b8c28936fa14be57bffbb6
hugosenari <hugosenari@gmail.com>
2015-08-03 00:47:21 -0200

    add first plugin: reddit

--- www/plugins/reddit.js
+++ www/plugins/reddit.js
@@ -6,39 +6,39 @@
 		return content.replace(/(href="[^"]+")/gi, '$1 target="_blank"');
 	}
 	
-	function findLink(content){
-		var link = content.replace(/.*<a[^>]+href="([^"]+)"[^>]*>\s*\[link\]\s*<\/a\s*>\s*<a[^>]+>\s*\[.*/gim, '$1');
-		return link;
-	}
-	
-	function linkType(link){
-		var domain = link.replace(/^https?:\/\/([^:?/]+)[:?/]*.*/i, '$1');
-		console.log(domain);
-		if (domain.match(/\.youtube\./i)) {
-			return function(content){
-				return content;
-			}
-		}
-		if (domain.match(/\.i\.imgur\./i)) {
-			return function(content){
-				return content;
-			}
-		}
-		return undefined;
-	}
+	//function findLink(content){
+	//	var link = content.replace(/.*<a[^>]+href="([^"]+)"[^>]*>\s*\[link\]\s*<\/a\s*>\s*<a[^>]+>\s*\[.*/gim, '$1');
+	//	return link;
+	//}
+	//
+	//function linkType(link){
+	//	var domain = link.replace(/^https?:\/\/([^:?/]+)[:?/]*.*/i, '$1');
+	//	console.log(domain);
+	//	if (domain.match(/\.youtube\./i)) {
+	//		return function(content){
+	//			return content;
+	//		}
+	//	}
+	//	if (domain.match(/\.i\.imgur\./i)) {
+	//		return function(content){
+	//			return content;
+	//		}
+	//	}
+	//	return undefined;
+	//}
     
     injector.invoke(['Plugins', function(Plugins) {
         Plugins.watch('before-show-article', function(args){
 			if (args && args.link && args.link.match('www.reddit.com')) {
 				args.link = args.link + '.compact'
-				//if (args.content) {
-				//	args.content = targetBlank(args.content);
+				if (args.content) {
+					args.content = targetBlank(args.content);
 				//	var link = findLink(args.content);
 				//	var type = linkType(link);
 				//	if (type) {
 				//		args.content = type(args.content);
 				//	}
-				//}
+				}
 			}
             return args;
         });
